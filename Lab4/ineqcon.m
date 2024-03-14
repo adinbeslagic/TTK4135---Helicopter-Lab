beeb2f043ea7b729b9bf99d3ed5f975aeb8cf16e
adinbeslagic <143542121+adinbeslagic@users.noreply.github.com>
2024-03-14 14:58:00 +0100

    cleanup

--- Lab4/ineqcon.m
+++
@@ -1,8 +0,0 @@
-function [c, ceq] = ineqcon(z)
-    global alpha beta lambda_t mx N
-    c = zeros(N, 1);
-    ceq = [];
-    for k = 1:N
-        c(k) = alpha*exp(-beta*(z(1+(k-1)*mx)-lambda_t)^2) - z(5+(k-1)*mx);
-    end
-end
